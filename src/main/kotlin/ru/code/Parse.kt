adfce1dc666a3919340fbdc5ec9cc71ded083964
4-o-4 <yuzhakov.lev@gmail.com>
2024-11-17 21:21:54 +0400

    feat: map extension

--- src/main/kotlin/ru/code/Parse.kt
+++ src/main/kotlin/ru/code/Parse.kt
@@ -6,38 +6,39 @@ const val UPPER_CASE_LENGTH = 1
 val lowercase = 'a'..'z'
 val uppercase = 'A'..'Z'
 
-fun parse(str: String) {
+fun MutableMap<String, Int>.parse(str: String) {
     val sb = StringBuilder()
     var state = OFF
+
+    fun put(sb: StringBuilder) = with(sb) {
+        if (this.length != 1)
+            compute(this.toString()) { _, v -> v?.inc() ?: 1 }
+        this.clear()
+    }
 
     for (c in str) {
         when (c) {
             in lowercase -> {
-                if (state == UPPER_CASE && sb.length != UPPER_CASE_LENGTH) {
-                    println(sb)
-                    sb.clear()
-                }
+                if (state == UPPER_CASE && sb.length != UPPER_CASE_LENGTH)
+                    put(sb)
                 sb.append(c)
                 state = LOWER_CASE
             }
 
             in uppercase -> {
-                if (state == LOWER_CASE) {
-                    println(sb)
-                    sb.clear()
-                }
+                if (state == LOWER_CASE)
+                    put(sb)
                 sb.append(c.lowercaseChar())
                 state = UPPER_CASE
             }
 
             else -> {
                 if (state != OFF) {
-                    println(sb)
-                    sb.clear()
+                    put(sb)
                     state = OFF
                 }
             }
         }
     }
-    if (state != OFF) println(sb)
+    if (state != OFF) put(sb)
 }
